97d2cd68d87c39722a348354dd8915a77e1be614
rlarldus <rlarldus4765@naver.com>
2020-10-11 17:04:30 +0900

    2020/10/11 게시물 수정(폼)기능 구현

--- src/main/java/com/sbs/example/lolHi/controller/usr/ArticleController.java
+++ src/main/java/com/sbs/example/lolHi/controller/usr/ArticleController.java
@@ -44,6 +44,13 @@ public class ArticleController {
 		return String.format("<script> alert('%d번 글을 삭제하였습니다.'); location.replace('/usr/article/list'); </script>", id);
 	}
 
+	@RequestMapping("/usr/article/modify")
+	public String showModify(Model model, int id) {
+		Article article = articleService.getArticleById(id);
+		model.addAttribute("article", article);
+		return "usr/article/modify";
+	}
+
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
 	public String doModify(int id, String title, String body) {
@@ -52,11 +59,12 @@ public class ArticleController {
 		return String.format(
 				"<script> alert('%d번 글을 수정하였습니다.'); location.replace('/usr/article/detail?id=%d'); </script>", id, id);
 	}
+
 	@RequestMapping("/usr/article/write")
 	public String showWrite() {
 		return "usr/article/write";
 	}
-	
+
 	@RequestMapping("/usr/article/doWrite")
 	@ResponseBody
 	public String doWrite(@RequestParam Map<String, Object> param) {
