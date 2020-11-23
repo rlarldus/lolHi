eefd3ad2fd18db96580ff5e41b81f597a79c8a87
rlarldus <rlarldus4765@naver.com>
2020-11-23 19:09:25 +0900

    회원테이블에 이메일 칼럼을 추가

--- dq.sql
+++ dq.sql
@@ -1,7 +1,7 @@
 # 데이터 베이스 생성
-DROP DATABASE IF EXISTS lolHi;
-CREATE DATABASE lolHi;
-USE lolHi;
+DROP DATABASE IF EXISTS lolHiT;
+CREATE DATABASE lolHiT;
+USE lolHiT;
 
 # 게시물 테이블 생성
 CREATE TABLE article (
@@ -25,14 +25,6 @@ updateDate = NOW(),
 title = '제목2',
 `body` = '내용2';
 
-CREATE TABLE `member` (
-    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    regDate DATETIME NOT NULL,
-    updateDate DATETIME NOT NULL,
-    loginId CHAR(20) NOT NULL,
-    loginPw CHAR(100) NOT NULL,
-    `name` CHAR(100) NOT NULL
-); 
 # 회원 테이블 생성
 CREATE TABLE `member` (
     id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -58,6 +50,26 @@ loginId = 'test2',
 loginPw = 'test2',
 `name` = '홍길순';
 
+# 게시물 테이블에 memberId 칼럼 추가
+ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER updateDate;
+# 기존 게시물들의 작성자는 1번 회원으로 정한다.
+UPDATE article SET memberId = 1 WHERE memberId = 0;
+
+# 게시물 데이터 추가
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목3',
+`body` = '내용3',
+memberId = 2;
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목4',
+`body` = '내용4',
+memberId = 2;
+
 # 댓글 테이블 생성
 CREATE TABLE reply (
     id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -68,6 +80,7 @@ CREATE TABLE reply (
     relId INT(10) UNSIGNED NOT NULL, # 관련 데이터 ID
     relTypeCode CHAR(50) NOT NULL # 관련 데이터 타입
 );
+
 # 댓글 데이터 추가
 INSERT INTO reply
 SET regDate = NOW(),
@@ -101,15 +114,6 @@ relId = 1,
 `body` = '댓글 4',
 memberId = 2;
 
-SELECT * FROM reply; 
-
-# 게시물 테이블에 memberId 칼럼 추가
-ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER updateDate;
-# 기존 게시물들의 작성자는 1번 회원으로 정한다.
-UPDATE article SET memberId = 1 WHERE memberId = 0;
-
-SELECT * FROM article;
-
 # 게시판 테이블 추가
 CREATE TABLE board (
     id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -135,3 +139,7 @@ updateDate = NOW(),
 ALTER TABLE article ADD COLUMN boardId INT(10) UNSIGNED NOT NULL AFTER updateDate;
 UPDATE article SET boardId = 1 WHERE id <= 2;
 UPDATE article SET boardId = 2 WHERE id > 2;
+
+# 회원테이블에 email 칼럼 추가
+ALTER TABLE MEMBER ADD COLUMN email CHAR(100) NOT NULL NOT NULL AFTER `name`;
+UPDATE `member` SET email = 'jangka512@gmail.com' WHERE email = '';
